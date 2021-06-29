d9b6ba09453f8da849b0f80b11cf026c23c26f9c
Jason Bailey <jmbailey816@gmail.com>
2021-06-28 20:14:08 -0400

    finished generate markdown page

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,61 @@
 // TODO: Include packages needed for this application
-const fs = require("fs");
-const inquirer = require("inquirer");
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: "input",
-    name: "github",
-    message: "what is your github name?",
+    type: 'input',
+    name: 'github',
+    message: 'what is your github name?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'what is your email?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "what is your project?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'write a description of your project',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'which license do you want to use?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'what command should be run to install dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'what command should be used to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'what does user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'what does the user need to know about contributing to repo?',
   },
 ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
