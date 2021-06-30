e62507a74dbe30af677ace19d3432940399ed619
Jason Bailey <jmbailey816@gmail.com>
2021-06-30 09:01:13 -0400

    file cleanup

--- assets/index.js
+++ assets/index.js
@@ -7,17 +7,17 @@ const questions = [
   {
     type: 'input',
     name: 'github',
-    message: 'what is your github name?',
+    message: 'what is your github name:',
   },
   {
     type: 'input',
     name: 'email',
-    message: 'what is your email?',
+    message: 'what is your email:',
   },
   {
     type: 'input',
     name: 'title',
-    message: "what is your project?",
+    message: "what is your project:",
   },
   {
     type: 'input',
@@ -27,30 +27,30 @@ const questions = [
   {
     type: 'list',
     name: 'license',
-    message: 'which license do you want to use?',
+    message: 'which license do you want to use:',
     choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
   },
   {
     type: 'input',
     name: 'installation',
-    message: 'what command should be run to install dependencies?',
+    message: 'what command should be run to install dependencies:',
     default: 'npm i',
   },
   {
     type: 'input',
     name: 'test',
-    message: 'what command should be used to run tests?',
+    message: 'what command should be used to run tests:',
     default: 'npm test',
   },
   {
     type: 'input',
     name: 'usage',
-    message: 'what does user need to know about using the repo?',
+    message: 'what does user need to know about using the repo:',
   },
   {
     type: 'input',
     name: 'contributing',
-    message: 'what does the user need to know about contributing to repo?',
+    message: 'what does the user need to know about contributing to repo:',
   },
 ];
 
